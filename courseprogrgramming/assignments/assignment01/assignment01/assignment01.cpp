7cb6302292ac2f7319a15b69bb37b4f6bf69b199
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2025-12-04 06:06:27 +0300

    updated

--- courseprogrgramming/assignments/assignment01/assignment01/assignment01.cpp
+++ courseprogrgramming/assignments/assignment01/assignment01/assignment01.cpp
@@ -3,10 +3,12 @@
 int main()
 {
     std::cout << "****************************\n";
-    std::cout << "Name:" << "Lama Aljohani\n";
-    std::cout << "Age : " << "23 years\n";
-    std::cout << "City : " << "Yanbu\n";
-    std::cout << "Country:" << "Saudi Arabia\n";
+    std::cout << "Name:   Lama Aljohani\n";
+    std::cout << "Age :  23 years\n";
+    std::cout << "City : Yanbu\n";
+    std::cout << "Country : Saudi Arabia\n";
     std::cout << "****************************\n";
+
+    return 0;
 
 }
--- courseprogrgramming/assignments/assignment03/assignment03/assignment03.cpp
+++ courseprogrgramming/assignments/assignment03/assignment03/assignment03.cpp
@@ -3,13 +3,13 @@
 
 int main()
 {
-    std::cout << "I Love Programming!\n\n";
-    std::cout << "I promise to be the best developer ever!\n\n";
-    std::cout << "I Know it will take some time to practice, but I will achive my goal.!\n\n";
+    std::cout << "I Love Programming!" << std::endl<<std::endl;
+    std::cout << "I promise to be the best developer ever!" << std::endl << std::endl;
+    std::cout << "I Know it will take some time to practice, but I will achive my goal.!" << std::endl << std::endl;
 
-    std::cout << "Best Regards,\n";
-    std::cout << "Lama Aljohani.\n";
+    std::cout << "Best Regards," << std::endl;
+    std::cout << "Lama Aljohani." << std::endl;
 
-
+    return 0;
 
 }
