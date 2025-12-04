18c17623e561f7a757cf44944dec29345064dba1
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2025-12-04 05:17:07 +0300

    Added assignment 01

---
+++ courseprogrgramming/assignments/assignment01/assignment01/assignment01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "****************************\n";
+    std::cout << "Name:" << "Lama Aljohani\n";
+    std::cout << "Age : " << "23 years\n";
+    std::cout << "City : " << "Yanbu\n";
+    std::cout << "Country:" << "Saudi Arabia\n";
+    std::cout << "****************************\n";
+
+}
