b10d6f1a6fa84c8d58633cc7f3e319622befbadc
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2025-12-04 05:31:18 +0300

    add assignment03

---
+++ courseprogrgramming/assignments/assignment03/assignment03/assignment03.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "I Love Programming!\n\n";
+    std::cout << "I promise to be the best developer ever!\n\n";
+    std::cout << "I Know it will take some time to practice, but I will achive my goal.!\n\n";
+
+    std::cout << "Best Regards,\n";
+    std::cout << "Lama Aljohani.\n";
+
+
+
+}
