8a9b2bdfe1fce7f58d9e429b51cb600c4c171655
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2025-12-27 21:09:21 +0300

    Added assignment 13&14

---
+++ courseprogrgramming/assignments/assignment13/assignment13/assignment13.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+    int A;
+    int B;
+
+
+
+    cout << "Please enter the first number A" << endl;
+    cin >> A;
+    cout << "Please enter the first number B" << endl;
+    cin >> B;
+
+    cout << "A+B=" << A + B << endl;
+    cout << "A-B=" << A - B << endl;
+    cout << "A*B=" << A * B << endl;
+    cout << "A/B=" << A / B << endl;
+    cout << "A%B=" << A % B << endl;
+
+
+    return 0;
+
+}
