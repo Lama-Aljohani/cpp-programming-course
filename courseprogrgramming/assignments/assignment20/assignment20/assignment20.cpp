6d6e5ec3f1245aa59ca8b888484c29408f4c1f0c
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2025-12-28 00:18:09 +0300

    Added assignment 20

---
+++ courseprogrgramming/assignments/assignment20/assignment20/assignment20.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+    int d;
+
+
+    cout << "Please enter  d" << endl;
+    cin >> d;
+
+ 
+    int Area = (3.14 *d*d)/4;
+
+    cout << "Area = " << Area << endl;
+
+    return 0;
+}
