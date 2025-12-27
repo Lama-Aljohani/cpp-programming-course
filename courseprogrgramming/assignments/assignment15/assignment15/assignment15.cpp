834f8359d9ab158ea0143c0f8ee60bf17a087523
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2025-12-27 21:15:32 +0300

    Added assignment 15

---
+++ courseprogrgramming/assignments/assignment15/assignment15/assignment15.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+    int number1;
+    int number2;
+    int number3;
+
+
+    cout << "Please enter the number1" << endl;
+    cin >> number1;
+
+    cout << "Please enter the number2" << endl;
+    cin >> number2;
+
+
+    cout << "Please enter the number3" << endl;
+    cin >> number3;
+
+
+    int sum = number1 + number2 + number3;
+
+
+    cout << "Sum = " << sum  << endl;
+
+    return 0;
+}
