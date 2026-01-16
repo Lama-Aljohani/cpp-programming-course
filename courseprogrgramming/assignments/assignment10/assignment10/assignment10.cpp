a1ac0b77f07914889595cb1d34fab11e8ab8c094
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-01-16 19:36:21 +0300

    Update assignment10

--- courseprogrgramming/assignments/assignment10/assignment10/assignment10.cpp
+++ courseprogrgramming/assignments/assignment10/assignment10/assignment10.cpp
@@ -1,50 +1,80 @@
-
 #include <iostream>
 using namespace std;
 
+
+
+struct ContactInformation {
+
+    string phone;
+    string email;
+
+};
+
+struct Personalemplpyee {
+
+    string Name;
+    int Age;
+    string City;
+    string Country;
+    float MonthlySalary;
+    char Gender;
+    bool isMarried;
+    ContactInformation EmployeeContact;
+};
+
+
 int main()
 {
-        string Name;
-        int Age;
-        string City;
-        string Country;
-        float MonthlySalary;
-        char Gender;
-        bool isMarried;
+    Personalemplpyee employee1;
+
+    employee1.Name;
+    employee1.Age;
+    employee1.City;
+    employee1.Country;
+    employee1.MonthlySalary;
+    employee1.Gender;
+    employee1.isMarried;
+    employee1.EmployeeContact.email;
+    employee1.EmployeeContact.phone;
 
         cout << "Please enter your Name?" << endl;
-        cin >> Name;
+        cin >> employee1.Name;
 
         cout << "Please enter your Age?" << endl;
-        cin >> Age;
+        cin >> employee1.Age;
 
         cout << "Please enter your City?" << endl;
-        cin >> City;
+        cin >> employee1.City;
 
         cout << "Please enter your Country?" << endl;
-        cin >> Country;
+        cin >> employee1.Country;
 
         cout << "Please enter your Monthly Salary?" << endl;
-        cin >> MonthlySalary;
-
+        cin >> employee1.MonthlySalary;
 
         cout << "Please enter your Gender ?" << endl;
-        cin >> Gender;
+        cin >> employee1.Gender;
 
+        cout << "Are you married  1\0 ?" << endl;
+        cin >> employee1.isMarried;
 
-        cout << "Are you married ? 1\0 ?" << endl;
-        cin >> isMarried;
+        cout << "Please enter your Email ?" << endl;
+        cin >> employee1.EmployeeContact.email;
 
+        cout << "Please enter your Phone ?" << endl;
+        cin >> employee1.EmployeeContact.phone;
 
         cout << "***************************\n";
-        cout << "Name: " << Name << endl;
-        cout << "Age: " << Age << " years." << endl;
-        cout << "City: " << City << endl;
-        cout << "Country: " << Country << endl;
-        cout << "Monthly Salary: " << MonthlySalary << endl;
-        cout << "Yearly Salary: " << MonthlySalary * 12 << endl;
-        cout << "Grnder: " << Gender << endl;
-        cout << "Married: " << isMarried << endl;
+        cout << "Name: " << employee1.Name << endl;
+        cout << "Age: " << employee1.Age << " years." << endl;
+        cout << "City: " << employee1.City << endl;
+        cout << "Country: " << employee1.Country << endl;
+        cout << "Monthly Salary: " << employee1.MonthlySalary << endl;
+        cout << "Yearly Salary: " << employee1.MonthlySalary * 12 << endl;
+        cout << "Grnder: " << employee1.Gender << endl;
+        cout << "Married: " << employee1.isMarried << endl;
+        cout << "Email: " << employee1.EmployeeContact.email << endl;
+        cout << "Phone: " << employee1.EmployeeContact.phone << endl;
         cout << "***************************\n";
 
         return 0;
