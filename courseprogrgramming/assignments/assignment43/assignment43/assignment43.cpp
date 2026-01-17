4bf3df6f8a0e991e0da614f375059697d7a29204
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-01-17 23:04:25 +0300

    update assignment 43

--- courseprogrgramming/assignments/assignment43/assignment43/assignment43.cpp
+++ courseprogrgramming/assignments/assignment43/assignment43/assignment43.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 
 int main()
 {
-    int Num1;
-    double Num2 = 18.99;
 
-    Num1 = Num2;        // Impalecit Conversion from double to in
+    string st1 = "43.22";
+    int N1 = 20;
+    double N2 = 33.5;
+    float N3 = 55.23;
 
-    Num1 = (int)Num2;  //Explicit Conversion
+    int num1_integer = stoi(st1);
+    double num2_double = stod(st1);
+    float num3_float = stof(st1);
+    
+    
+    string SN1 , SN2 , SN3;
 
-    Num1 = int (Num2); //Explicit Conversion
+    SN1 = to_string(N1);
+    SN2 = to_string(N2);
+    SN3 = to_string(N3);
 
 
-    cout << Num1 << endl;
+    N3 = int(N3);
+
+
+    cout <<"num1_integer = " << num1_integer << endl;
+    cout <<"num2_double = " << num2_double << endl;
+    cout <<"num3_float =" << num3_float << endl;
+    cout <<"convert int to string =" << SN1 << endl;
+    cout << "convert double to string =" << SN2 << endl;
+    cout << "convert float to string =" << SN3 << endl;
+    cout << "convert float to int =" << N3 << endl;
+
     return 0;
+
 }
