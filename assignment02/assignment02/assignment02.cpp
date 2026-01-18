648b859014d2acb7fbff883e0e4ddff25c15afbe
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-01-18 09:09:10 +0300

    Update assignments 1&2&3&4
    Refactored assignments by implementing functions to improve code structure, readability, and reusability.

---
+++ assignment02/assignment02/assignment02.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+void PrintSequrStars(){
+
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n";
+
+}
+int main()
+{
+    PrintSequrStars();
+
+    return 0;
+
+}
+
---
+++ assignment04/assignment04/assignment04.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void PrintH() {
+
+    cout << "*   *" << endl;
+    cout << "*   *" << endl;
+    cout << "*****" << endl;
+    cout << "*   *" << endl;
+    cout << "*   *" << endl;
+}
+
+int main()
+{
+    PrintH();
+    return 0;
+
+ 
+}
