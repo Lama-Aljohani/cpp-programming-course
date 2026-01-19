da54249c3c72c25aabd1dcec9af517a584ae9ede
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-01-19 09:45:30 +0300

    Update assignment17

--- courseprogrgramming/assignments/assignment17/assignment17/assignment17.cpp
+++ courseprogrgramming/assignments/assignment17/assignment17/assignment17.cpp
@@ -4,31 +4,32 @@
 using namespace std;
 
 
-int swapNumber(int Number1, int Number2) {
-
-    cout << "Nubmer1 = " << Number1 << endl;
-    cout << "Nubmer2 = " << Number2 << endl << endl;
-     
+void swapNumber(int &Number1, int &Number2) {
 
     int Temp;
     Temp = Number1;
     Number1 = Number2;
     Number2 = Temp;
 
+    cout << "After Swap inside function Number1 = " << Number1 << " , Number2=" << Number2 <<endl << endl;
+     
     cout << "************************************" << endl;
-
-    cout << "Nubmer1 = " << Number1 << endl;
-    cout << "Nubmer2 = " << Number2 << endl;
-
-    return Number1 , Number2;
-
 }
-
 
 int main()
 {
+    int Number1, Number2;
 
-    cout << swapNumber (5 , 6) << endl;
+    cout << "Please enter the Number1? \n = "  << endl;
+    cin >> Number1;
+
+    cout << "Please enter the Number2? \n = " << endl;
+    cin >> Number2;
+ 
+
+    swapNumber(Number1, Number2);
+
+    cout << "After Swap inside Main the function Number1 = " << Number1 << ", Number2=" << Number2 << endl;
 
     return 0;
 }
