bb3beacfb369d4195899d251d9a9d7faf2091557
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-01-26 08:40:36 +0300

    Update assignment48

--- courseprogrgramming/assignments/assignment48/assignment48/assignment48.cpp
+++ courseprogrgramming/assignments/assignment48/assignment48/assignment48.cpp
@@ -3,24 +3,35 @@
 using namespace std;
 
 
-int main()
-{
-	float Grades[3];
+void ReadGrades(float Grades[3]) {
 
-	cout << "Please Enter Grade1? \n" ;
-	cin >> Grades[0]; 
+
+	cout << "Please Enter Grade1? \n";
+	cin >> Grades[0];
 
 	cout << "Please Enter Grade2? \n";
 	cin >> Grades[1];
 
-	cout << "Please Enter Grade3? \n" ;
+	cout << "Please Enter Grade3? \n";
 	cin >> Grades[2];
+}
+void PrintGrades(float Grades[3]) {
+
 
 	float Avg = (Grades[0] + Grades[1] + Grades[2]) / 3;
 
 	cout << "********************************** " << endl;
 
 	cout << "The average of grades is " << Avg << endl;
+
+}
+
+int main()
+{
+	float Grades[3];
+
+	ReadGrades(Grades);
+	PrintGrades(Grades);
 
 	return 0;
 
