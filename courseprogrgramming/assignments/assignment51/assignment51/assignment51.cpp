0041a7310516e63f92c5f8575705bb07e2de276e
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-01-27 19:00:20 +0300

    Added assignment 51

---
+++ courseprogrgramming/assignments/assignment51/assignment51/assignment51.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	int Mark;
+
+	cout << " Please Enter your Mark " << endl;
+	cin >> Mark;
+
+	if (Mark > 50)
+	{
+
+		cout << " Pass " << endl;
+
+	}
+	else
+	{
+		cout << " Fail " << endl;
+
+	}
+
+	return 0;
+
+}
