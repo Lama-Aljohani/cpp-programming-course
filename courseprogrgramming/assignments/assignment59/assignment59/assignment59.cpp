0a959fec64cc5da5d41bc9cf456a917c78c4f85d
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-01-31 22:49:41 +0300

    Added assignment 59

---
+++ courseprogrgramming/assignments/assignment59/assignment59/assignment59.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int Month;
+
+	cout << "Please Enter Month " << endl;
+	cin >> Month;
+
+	if (Month == 1) {
+
+		cout << "January" << endl;
+
+	}
+	else if (Month == 2) {
+
+		cout << "February" << endl;
+
+	}
+	else if (Month == 3) {
+
+		cout << "March" << endl;
+
+	}
+	else if (Month == 4) {
+
+		cout << "April" << endl;
+
+	}
+
+	else if (Month == 5) {
+
+		cout << "May" << endl;
+
+	}
+
+	else if (Month == 6) {
+
+		cout << "June" << endl;
+
+	}
+
+	else if (Month == 7) {
+
+		cout << "July" << endl;
+
+	}
+
+	else if (Month == 8) {
+
+		cout << "August" << endl;
+
+	}
+
+
+	else if (Month == 9) {
+
+		cout << "September" << endl;
+
+	}
+
+
+	else if (Month == 10) {
+
+		cout << "October" << endl;
+
+	}
+
+	else if (Month == 11) {
+
+		cout << "November" << endl;
+
+	}
+
+	else if (Month == 12) {
+
+		cout << "December" << endl;
+
+	}
+
+	else
+	{
+		cout << "Wrong Month" << endl;
+
+	}
+
+	return 0;
+
+}
