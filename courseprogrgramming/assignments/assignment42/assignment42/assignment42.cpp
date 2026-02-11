4a164e7d2a4c63e1a0b84854b83939245811e214
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-02-11 18:56:51 +0300

    update assignment 42

--- courseprogrgramming/assignments/assignment42/assignment42/assignment42.cpp
+++ courseprogrgramming/assignments/assignment42/assignment42/assignment42.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
+
+void ReadinputUser(double &Number, double &M) {
+
+    cout << "Please enter the Number " << endl;
+    cin >> Number;
+    cout << "Please enter the M " << endl;
+    cin >> M;
+    cout << "*************************" << endl;
+
+}
+
+double pownumberM(double Number,double M) {
+    
+    double result = 1;
+
+    for (int C = 1; C <= M; C++) {
+
+        result = result * Number;
+    }
+    return result;
+
+}
 
 int main()
 {
-    double Number;
+    double Number; 
     double M;
-
-    cout << "please enter the Number " << endl;
-    cin >> Number;
-
-
-    cout << "please enter the M " << endl;
-    cin >> M;
-
-    cout << endl;
-
-
-    double result = pow(Number, M);
-    double finalResult = round(result);
-
-    cout << "Result = " << finalResult << endl;
-
-
-
-
-    //cout << "Result =  " << round( pow(Number, M)) << endl;
-
-
+    ReadinputUser(Number, M);
+    cout << "Result = " << pownumberM(Number, M) << endl;
 
     return 0;
 }
