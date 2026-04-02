767c14ddf2c02a88d20154b6b38bc90ba8f29b2d
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-04-02 21:19:11 +0300

    Update assignment 19

--- courseprogrgramming/assignments/assignment19/assignment19/assignment19.cpp
+++ courseprogrgramming/assignments/assignment19/assignment19/assignment19.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 using namespace std;
 
+void ReadNumbers(float& A, float& H) {
 
+    cout << "Please enter the tringle side A ? " << endl;
+    cin >> A;
+
+    cout << "Please enter the tringle side H ? " << endl;
+    cin >> H;
+
+}
+
+float calculateTringleArea(float A, float H) {
+
+    float Area = (A / 2) * H;
+
+    return Area;
+
+}
+
+void PrintResult(float Area) {
+
+    cout << "\nTringle Area = " << Area << endl;
+
+}
 
 int main()
 {
 
-    int a;
-    int h;
+    float A, H;
+    ReadNumbers(A, H);
+    PrintResult(calculateTringleArea(A, H));
 
-
-    cout << "Please enter  a" << endl;
-    cin >> a;
-
-    cout << "Please enter  b" << endl;
-    cin >> h;
-    int Area = a/2* h;
-
-    cout << "Area = " << Area << endl;
 
     return 0;
 }
