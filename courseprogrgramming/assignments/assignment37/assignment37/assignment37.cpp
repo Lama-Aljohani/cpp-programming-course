443713f32d481f2612f7b35469a9d0d66d53dd3d
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-04-04 02:26:43 +0300

    Update assignment 37

--- courseprogrgramming/assignments/assignment37/assignment37/assignment37.cpp
+++ courseprogrgramming/assignments/assignment37/assignment37/assignment37.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
+
+
+float ReadSquare() {
+
+    float A;
+    cout << "Please enter radious A ? " << endl;
+    cin >> A;
+
+    return A;
+}
+
+
+float CircleAreaInscribedInSquare(float A)
+
+{
+    const float  PI = 3.141592653589793238;
+
+    float Area = (pow(A, 2) * PI) / 4;
+
+    return Area;
+
+}
+
+
+void PrintResult(float Area) {
+
+  cout << "\nCircle Area = " << Area << endl;
+
+}
 
 int main()
 {
-    double A;
-    const double  PI = 3.1416;
 
-
-    cout << "please enter the number A" << endl;
-    cin >> A;
-
-
-    double area = PI * pow(A, 2) / 4;
-    double finalresult = ceil(area);
-
-    cout << endl;
-
-    cout << "finalresult " << finalresult << endl;
+    PrintResult(CircleAreaInscribedInSquare(ReadSquare()));
 
     return 0;
 }
