c6c74b8552ab2db9cf82fcb060ea7fc5f6d5eaaa
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-04-08 09:50:15 +0300

    Update assignment 61

--- courseprogrgramming/assignments/assignment61/assignment61/assignment61.cpp
+++ courseprogrgramming/assignments/assignment61/assignment61/assignment61.cpp
@@ -1,24 +1,62 @@
 #include <iostream>
 using namespace std;
 
-int ReadNumber(int& N) {
+int ReadNumber() {
+
+	int Number;
 	cout << "Please enter the Number " << endl;
-	cin >> N;
+	cin >> Number;
 	cout << "*************************" << endl;
 
-	int C = N;
-	while(C>=1) {
-	
-		cout << C << endl;
-		C--;
+	return Number;
+}
+
+void PrintRangeFromNto1_usingWhile(int N) {
+
+	int Counter = N+1;
+
+	cout << "Range printed using while Statement:\n";
+
+	while (Counter > 1)
+	{
+		Counter--;
+		cout << Counter << endl;
 	}
-	return N;
-} 
+}
+
+void PrintRangeFromNto1_usingDoWhile(int N) {
+
+	int Counter =N+1;
+
+	cout << "Range printed using Do..while Statement:\n";
+	do {
+
+		Counter--;
+		cout << Counter << endl;
+
+	} while (Counter > 1);
+
+}
+
+void PrintRangeFromNto1_usingFor(int N) {
+
+	cout << "Range printed using For Statement:\n";
+
+	for (int Counter = N; Counter >= 1; Counter--)
+	{
+		cout << Counter << endl;
+	}
+}
+
 int main()
 
 {
-	int N;
-	ReadNumber(N);
+	int N = ReadNumber();
+
+	PrintRangeFromNto1_usingWhile(N);
+	PrintRangeFromNto1_usingDoWhile(N);
+	PrintRangeFromNto1_usingFor(N);
+
 
 	return 0;
 
