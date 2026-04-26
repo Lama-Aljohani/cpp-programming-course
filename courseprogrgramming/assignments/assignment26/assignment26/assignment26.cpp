91e78699ee866c4bec3895401c3c393aca24e8c8
Lama-Aljohani <133781436+Lama-Aljohani@users.noreply.github.com>
2026-04-26 18:11:07 +0300

    Update assignment 26

--- courseprogrgramming/assignments/assignment26/assignment26/assignment26.cpp
+++ courseprogrgramming/assignments/assignment26/assignment26/assignment26.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 using namespace std;
 
+float ReadPositiveNumber(string Message) {
+
+    float Number = 0;
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+float TotalBillAfterServiceAndTax(float TotalBill)
+{
+    TotalBill = TotalBill * 1.1;
+    TotalBill = TotalBill * 1.16;
+
+    return TotalBill;
+}
+
 int main()
 {
 
-    float BillValue;
-    float totalbill;
-
-
-    cout << "Please enter the BillValue" << endl;
-    cin >> BillValue;
-
-     totalbill= BillValue * 1.1;
-     totalbill = totalbill * 1.16;
-
-
+    float TotalBill = ReadPositiveNumber("Plwase Enter Total Bill?");
 
     cout << endl;
-    cout << totalbill << endl;
+
+    cout << "Total Bill = " << TotalBill << endl;
+    cout << "Total Bill After Service  Fee and Sales Tax = "
+        << TotalBillAfterServiceAndTax(TotalBill) << endl;
+
 
     return 0;
 }
